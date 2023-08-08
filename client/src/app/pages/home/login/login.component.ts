fb2fb31af612c3bb3541d55982761b150a6025a1
cdine <celine.dine@yahoo.fr>
2023-08-08 10:46:26 +0200

    Global style sheet

--- client/src/app/pages/home/login/login.component.ts
+++ client/src/app/pages/home/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css', '../../../style/dialog.css']
+    styleUrls: ['./login.component.css', '../../../styles/dialog.css']
   })
   export class LoginComponent {
     username: string = "";
