53d215bfd865670fc81898f5c2d8f21ddb19ebb2
cdine <celine.dine@yahoo.fr>
2023-08-09 18:35:17 +0200

    Changed database init

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,90 +1,108 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS "tag" (
-    "id" SERIAL PRIMARY KEY,
+    "id" VARCHAR(50) PRIMARY KEY DEFAULT uuid_generate_v4(),
     "label" VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "profile" (
-    "id" SERIAL PRIMARY KEY,
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "gender" VARCHAR(100) DEFAULT 'Not specified',
     "birth_date" DATE,
     "sexual_preferences" VARCHAR(100) DEFAULT 'bisexual',
     "biography" VARCHAR(510),
     "location" VARCHAR(100),
-    "fame_rate" INTEGER DEFAULT 100
+    "fame_rate" INTEGER DEFAULT 100,
+    "default_picture_id" UUID,
+    "user_id" UUID NOT NULL,
+    FOREIGN KEY ("default_picture_id") REFERENCES "picture"("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "profile_tag" (
-    "profile_id" INTEGER NOT NULL,
-    "tag_id" INTEGER NOT NULL,
-    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY ("tag_id") REFERENCES "tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+CREATE TABLE IF NOT EXISTS "profile_tag_asso" (
+    "profile_id" UUID NOT NULL,
+    "tag_id" UUID NOT NULL,
+    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("tag_id") REFERENCES "tag"("id") ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY ("profile_id", "tag_id")
 );
 
 CREATE TABLE IF NOT EXISTS "picture" (
-    "id" SERIAL PRIMARY KEY,
-    "profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "url" VARCHAR(255) NOT NULL
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "profile_id" UUID NOT NULL,
+    "url" VARCHAR(255) NOT NULL,
+    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "profile_default_picture" (
-    "profile_id" INTEGER NOT NULL,
-    "picture_id" INTEGER NOT NULL,
-    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY ("picture_id") REFERENCES "picture"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+CREATE TABLE IF NOT EXISTS "profile_picture_asso" (
+    "profile_id" UUID NOT NULL,
+    "picture_id" UUID NOT NULL,
+    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("picture_id") REFERENCES "picture"("id") ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY ("profile_id", "picture_id")
 );
 
 CREATE TABLE IF NOT EXISTS "like" (
-    "id" SERIAL PRIMARY KEY,
-    "profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "liked_profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "date" DATE DEFAULT CURRENT_TIMESTAMP
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "profile_id" UUID NOT NULL,
+    "liked_profile_id" UUID NOT NULL,
+    "date" DATE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("liked_profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "view" (
-    "id" SERIAL PRIMARY KEY,
-    "profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "viewed_profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "date" DATE DEFAULT CURRENT_TIMESTAMP
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "profile_id" UUID NOT NULL,
+    "viewed_profile_id" UUID NOT NULL,
+    "date" DATE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("viewed_profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "blacklist" (
-    "id" SERIAL PRIMARY KEY,
-    "profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "blacklisted_profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "date" DATE DEFAULT CURRENT_TIMESTAMP
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "profile_id" UUID NOT NULL,
+    "blacklisted_profile_id" UUID NOT NULL,
+    "date" DATE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("blacklisted_profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "fake_report" (
-    "id" SERIAL PRIMARY KEY,
-    "profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "fake_reported_profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "date" DATE DEFAULT CURRENT_TIMESTAMP
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "profile_id" UUID NOT NULL,
+    "fake_reported_profile_id" UUID NOT NULL,
+    "date" DATE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("fake_reported_profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "user" (
-    "id" SERIAL PRIMARY KEY,
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "last_name" VARCHAR(100) NOT NULL,
     "first_name" VARCHAR(100) NOT NULL,
     "email" VARCHAR(255) NOT NULL UNIQUE,
-    "password" VARCHAR(255) NOT NULL,
-    "profile_id" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE
+    "password" VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "messages" (
-    "id" SERIAL PRIMARY KEY,
-    "from" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "to" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "from_profile_id" UUID NOT NULL,
+    "to_profile_id" UUID NOT NULL,
     "message" VARCHAR(510) NOT NULL,
-    "date" DATE DEFAULT CURRENT_TIMESTAMP
+    "date" DATE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("from_profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("to_profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "notification" (
-    "id" SERIAL PRIMARY KEY,
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "is_viewed" BOOLEAN DEFAULT FALSE,
-    "from" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
-    "to" INTEGER REFERENCES "profile"("id") ON DELETE CASCADE,
+    "from_profile_id" UUID NOT NULL,
+    "to_profile_id" UUID NOT NULL,
     "type" VARCHAR(510) NOT NULL,
-    "date" DATE DEFAULT CURRENT_TIMESTAMP
+    "date" DATE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("from_profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("to_profile_id") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
