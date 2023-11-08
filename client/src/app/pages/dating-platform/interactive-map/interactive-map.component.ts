c2a0b32d81538de39727d43418cf263521d2a4d2
Celine DINE <cdine@paul-f5Ar3s7.clusters.42paris.fr>
2023-11-08 16:30:01 +0100

    Added popup

--- client/src/app/pages/dating-platform/interactive-map/interactive-map.component.ts
+++ client/src/app/pages/dating-platform/interactive-map/interactive-map.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { MapUser, User } from "@shared-models/user.model";
+import { GeoCoordinate, MapUser, User } from "@shared-models/user.model";
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
@@ -12,11 +12,14 @@ import Point from 'ol/geom/Point'
 import Feature from 'ol/Feature.js';
 import Icon from 'ol/style/Icon';
 import Style from 'ol/style/Style';
+import Overlay from 'ol/Overlay';
+import { Router } from "@angular/router";
+import { getFirebasePictureUrl } from "src/app/utils/picture.utils";
 
 @Component({
 	selector: 'app-interactive-map',
 	templateUrl: './interactive-map.component.html',
-	styleUrls: ['./interactive-map.component.css', '../../../styles/buttons.css']
+	styleUrls: ['./interactive-map.component.css', '../../../styles/picture.css']
 })
 export class InteractiveMapComponent {
 
@@ -26,8 +29,11 @@ export class InteractiveMapComponent {
 
 	mySubscriptions: Subscription[] = [];
 
+	selectedFeature: any = null;
+
 	constructor(
-		private userService: UserService
+		private userService: UserService,
+		private router: Router
 	) { }
 
 	ngOnInit() {
@@ -37,10 +43,24 @@ export class InteractiveMapComponent {
 					this.currentUser = user;
 					if (this.map)
 						this.updateMap();
+					this.setView();
 				}
 			})
 		);
 		this.initMap();
+	}
+
+	setView() {
+		this.map.setView(new View({
+			center: [
+				this.currentUser?.userGivenLocation?.longitude || this.currentUser?.location?.longitude || 0,
+				this.currentUser?.userGivenLocation?.latitude || this.currentUser?.location?.latitude || 0
+			],
+			projection: 'EPSG:4326',
+			zoom: 18,
+			maxZoom: 19,
+		}));
+		this.listenToZoom();
 	}
 
 	async initMap() {
@@ -50,19 +70,60 @@ export class InteractiveMapComponent {
 					source: new OSM(),
 				}),
 			],
-			target: 'map',
-			view: new View({
-				center: [
-					this.currentUser?.userGivenLocation?.longitude || this.currentUser?.location?.longitude || 0,
-					this.currentUser?.userGivenLocation?.latitude || this.currentUser?.location?.latitude || 0
-				],
-				projection: 'EPSG:4326',
-				zoom: 18,
-				maxZoom: 19,
-			}),
+			target: 'map'
 		});
+		this.setView();
 		await this.getOtherUsers();
 		this.addUsersPins();
+		this.setPopup();
+		this.listenToZoom();
+	}
+
+	setPopup() {
+		const popup = document.getElementById('popup');
+
+		if (!popup) return;
+		const popupOverlay = new Overlay({
+			element: popup as HTMLElement,
+			autoPan: {
+				animation: {
+					duration: 250,
+				},
+			},
+		});
+		this.map.addOverlay(popupOverlay);
+
+		this.map.on('click', () => {
+			popupOverlay.setPosition(undefined);
+			popup?.blur();
+		});
+		this.map.on('singleclick', (event) => {
+			if (this.selectedFeature !== null)
+				this.selectedFeature = null;
+			this.map.forEachFeatureAtPixel(event.pixel, (feature) => {
+				this.selectedFeature = feature;
+				return true;
+			});
+
+			if (!popup || !this.selectedFeature) return;
+			const user = this.otherUsers.find(user => user.id === this.selectedFeature.getProperties()['name']);
+			
+			(document.getElementById('popup-user-details') as HTMLElement).innerHTML = `
+				<span class="popup-user-name">${user?.username}</span>
+				<span>${user?.fameRate}%</span>
+			`;
+			(document.getElementById('popup-picture') as HTMLElement).innerHTML = `
+				<img src="${getFirebasePictureUrl(user?.pictureId)}" class="popup-picture" />
+			`;
+			popupOverlay.setPosition(event.coordinate);
+		});
+	}
+
+	listenToZoom() {
+		this.map.getView().on('change', () => {
+			this.getOtherUsers();
+			this.updateMap();
+		});
 	}
 
 	async getOtherUsers() {
@@ -76,13 +137,15 @@ export class InteractiveMapComponent {
 			})
 		);
 	}
-	
+
 	ngOnDestroy() {
 		this.mySubscriptions.forEach(subscription => subscription.unsubscribe());
 	}
 
 	updateMap() {
-		this.map.getLayers().clear();
+		this.map.getLayers().getArray()
+			.filter(layer => layer.getClassName() === 'currentUserLayer' || layer.getClassName() === 'otherUsersLayer')
+			.forEach(layer => this.map.removeLayer(layer));
 		this.addUsersPins();
 	}
 
@@ -97,7 +160,8 @@ export class InteractiveMapComponent {
 				this.currentUser?.userGivenLocation?.longitude || this.currentUser?.location?.longitude || 0,
 				this.currentUser?.userGivenLocation?.latitude || this.currentUser?.location?.latitude || 0
 			])),
-			projection: 'EPSG:4326'
+			projection: 'EPSG:4326',
+			name: 'currentUser'
 		});
 		pinFeature.setStyle(
 			new Style({
@@ -109,8 +173,9 @@ export class InteractiveMapComponent {
 		);
 		const vectorLayer = new VectorLayer({
 			source: new VectorSource({
-				features: [ pinFeature ],
+				features: [pinFeature],
 			}),
+			className: 'currentUserLayer'
 		});
 		this.map.addLayer(vectorLayer);
 	}
@@ -124,6 +189,7 @@ export class InteractiveMapComponent {
 					user.location.latitude
 				])),
 				projection: 'EPSG:4326',
+				name: user.id
 			}));
 		});
 		featuresArray.forEach(feature => feature.setStyle(new Style({
@@ -136,7 +202,14 @@ export class InteractiveMapComponent {
 			source: new VectorSource({
 				features: featuresArray,
 			}),
+			className: 'otherUsersLayer'
 		});
 		this.map.addLayer(vectorLayer);
+	}
+
+	navigateToProfile() {
+		const user = this.otherUsers.find(user => user.id === this.selectedFeature.getProperties()['name']);
+		if (!user) return;
+		this.router.navigate([`/app/profile`], { queryParams: { id: user.id } });
 	}
 }
