993df6bc3c9c79b1cc076c74ce0c48ba1802fc8d
Robin Bicanic <rbicanic@MBP-de-robin.lan>
2024-03-17 22:59:53 +0100

    push

--- client/src/app/pages/dating-platform/chat/chat.component.ts
+++ client/src/app/pages/dating-platform/chat/chat.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+	selector: 'app-chat',
+	templateUrl: './chat.component.html',
+	styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
-	constructor() {
+	isMobile!: Boolean;
+	isSideBarOpen!: Boolean;
+	constructor() { }
+
+	ngOnInit() {
+		if (window.screen.width <= 600) {
+			this.isMobile = true
+			this.isSideBarOpen = false;
+		} else {
+			this.isMobile = false;
+			this.isSideBarOpen = true;
+		}
+	}
+
+	@HostListener('window:resize', ['$event'])
+	onResize(event: any) {
+		const menu = document.getElementById('contact-side-bar');
+		if (event.target.innerWidth <= 600) {
+			this.isMobile = true
+			this.isSideBarOpen = false;
+			(menu as any).style.display = 'none'
+		} else {
+			this.isMobile = false;
+			this.isSideBarOpen = true;
+			(menu as any).style.display = 'block';
+		}
+	}
+
+	toggleMenu() {
+		const menu = document.getElementById('contact-side-bar');
+		(this.isSideBarOpen)
+			? (menu as any).style.display = 'none'
+			: (menu as any).style.display = 'block';
+		this.isSideBarOpen = !this.isSideBarOpen;
 	}
 }
--- client/src/app/pages/dating-platform/user-list/user-list-filters/user-list-filters.component.ts
+++ client/src/app/pages/dating-platform/user-list/user-list-filters/user-list-filters.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, Output, SimpleChanges } from "@angular/core";
+import { Component, EventEmitter, Input, Output, SimpleChanges, HostListener } from "@angular/core";
 import { ProfileFilters, UserListFilters } from "@shared-models/user.model";
 import { BehaviorSubject } from "rxjs";
 
 @Component({
 	selector: 'app-user-list-filters',
 	templateUrl: './user-list-filters.component.html',
-	styleUrls: ['./user-list-filters.component.css']
+	styleUrls: ['./user-list-filters.component.css', '../../../../styles/buttons.css']
 })
 export class UserListFiltersComponent {
 
@@ -16,6 +16,46 @@ export class UserListFiltersComponent {
 	age = UserListFilters.age;
 	fame = UserListFilters.fame;
 	distance = UserListFilters.distance;
+
+	isMobile!: Boolean;
+	isFilterMenuOpen!: Boolean;
+
+	ngOnInit() {
+		if (window.screen.width <= 800) {
+			this.isMobile = true;
+			this.isFilterMenuOpen = false;
+		} else {
+			this.isMobile = false;
+			this.isFilterMenuOpen = true;
+		}
+	}
+
+	@HostListener('window:resize', ['$event'])
+	onResize(event: any) {
+		const menu = document.getElementById('filters-settings-container');
+		if (event.target.innerWidth <= 800) {
+			this.isMobile = true;
+			this.isFilterMenuOpen = false;
+			(menu as any).style.display = 'none';
+		} else {
+			this.isMobile = false;
+			this.isFilterMenuOpen = true;
+			(menu as any).style.display = 'block';
+		}
+	}
+	
+	toggleMenu() {
+		const menu = document.getElementById('filters-settings-container');
+		const buttonDrawer = document.getElementById('filter-button-drawer');
+		if (this.isFilterMenuOpen) {
+			(menu as any).style.display = 'none';
+			(buttonDrawer as any).style.display = 'flex';
+		} else {
+			(menu as any).style.display = 'block';
+			(buttonDrawer as any).style.display = 'none';
+		}
+		this.isFilterMenuOpen = !this.isFilterMenuOpen;
+	}
 
 	ngOnChanges(changes: SimpleChanges) {
 		if (changes['filters'])
@@ -66,3 +106,39 @@ export class UserListFiltersComponent {
 		this.addedFilter.emit(this.filters);
 	}
 }
+
+// isMobile!: Boolean;
+// isSideBarOpen!: Boolean;
+// constructor() { }
+
+// ngOnInit() {
+// 	if (window.screen.width <= 600) {
+// 		this.isMobile = true
+// 		this.isSideBarOpen = false;
+// 	} else {
+// 		this.isMobile = false;
+// 		this.isSideBarOpen = true;
+// 	}
+// }
+
+// @HostListener('window:resize', ['$event'])
+// onResize(event: any) {
+// 	const menu = document.getElementById('contact-side-bar');
+// 	if (event.target.innerWidth <= 600) {
+// 		this.isMobile = true
+// 		this.isSideBarOpen = false;
+// 		(menu as any).style.display = 'none'
+// 	} else {
+// 		this.isMobile = false;
+// 		this.isSideBarOpen = true;
+// 		(menu as any).style.display = 'block';
+// 	}
+// }
+
+// toggleMenu() {
+// 	const menu = document.getElementById('contact-side-bar');
+// 	(this.isSideBarOpen)
+// 		? (menu as any).style.display = 'none'
+// 		: (menu as any).style.display = 'block';
+// 	this.isSideBarOpen = !this.isSideBarOpen;
+// }
