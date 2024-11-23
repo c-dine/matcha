1aba78b685f2e0983bdb82cab005794f0b3e0587
Celine Dine <celine.dine@yahoo.fr>
2024-11-23 16:40:51 +0100

    Link from card + fixed socket

--- client/src/app/pages/dating-platform/dating/dating-card-description/dating-card-description.component.ts
+++ client/src/app/pages/dating-platform/dating/dating-card-description/dating-card-description.component.ts
@@ -1,30 +1,41 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-dating-card-description',
-  templateUrl: './dating-card-description.component.html',
-  styleUrls: [
-    './dating-card-description.component.css',
-    '../dating.component.css'
-  ]
+	selector: 'app-dating-card-description',
+	templateUrl: './dating-card-description.component.html',
+	styleUrls: [
+		'./dating-card-description.component.css',
+		'../dating.component.css'
+	]
 })
 export class DatingCardDescriptionComponent {
-  @Input()
-  firstName!: string;
+	@Input()
+	firstName!: string;
 
-  @Input()
-  lastName!: string;
+	@Input()
+	id!: string | undefined;
 
-  @Input()
-  distance!: number | undefined;
+	@Input()
+	lastName!: string;
 
-  @Input()
-  biography!: string;
+	@Input()
+	distance!: number | undefined;
 
-  @Output()
-  clicked: EventEmitter<any> = new EventEmitter();
+	@Input()
+	biography!: string;
 
-  receiveClickEvent(name: string): void {
-    this.clicked.emit(name);
-  }
+	@Output()
+	clicked: EventEmitter<any> = new EventEmitter();
+
+	constructor(private router: Router) { }
+
+	receiveClickEvent(name: string): void {
+		this.clicked.emit(name);
+	}
+
+	goToProfile() {
+		if (this.id)
+			this.router.navigate([`/app/profile`], { queryParams: { id: this.id } });
+	}
 }
